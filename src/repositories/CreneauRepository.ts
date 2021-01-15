a40416711eac10c833b21e3b558738ccd018180f
RedWings-R <yves.henot01@gmail.com>
2021-01-15 09:08:43 +0100

    Crenau

---
+++ src/repositories/CreneauRepository.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const creneauSchema = new mongoose.Schema({
+    heure:String
+})
+
+
+const creneauModel = mongoose.model('creneau', creneauSchema);
+
+
+export class CreneauRepository {
+    public findAll() {
+        return creneauModel.find().exec();
+    }
+
+    public findById(id: string) {
+        return creneauModel.findById(id).exec();
+    }
+    public save(patient: any) {
+        return new creneauModel(patient).save();
+    }
+
+    public deleteById(id: string) {
+        return creneauModel.findByIdAndDelete(id);
+    }
+
+    // public findByName(name: string) {
+    //     return creneauModel.find({'nomMedecin':name});
+    // }
+
+    public findByIdAndUpdate(id: string, creneau: any) {
+        return creneauModel.findOneAndUpdate({'_id': id}, creneau);
+    }
+
+
+}    
+export const creneauRepository = Object.freeze(new CreneauRepository)
